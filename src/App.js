f43bb8eb391fd085f942e687ce8999dbcb6be60a
Jéssica Neri <jess.neri94@gmail.com>
2020-11-15 12:39:27 -0300

    Criação de rotas iniciadas.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./Login/Login";
+import Routes from "./Routes.js";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes />
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+/* páginas importadas */
+import Login from "./Login/Login";
+
+export default function Routes() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
